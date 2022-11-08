37d0123d1d82b1bec052542a45f05509b18ab8c7
Raaay-lab <pasha-zakharov-14@mail.ru>
2022-11-09 03:18:24 +0400

    cs-go code + attrs

--- cs-go settings/js/script.js
+++ cs-go settings/js/script.js
@@ -389,3 +389,191 @@ if (crosshair.dot === 1) {
     createDot(crosshair, center);
 }
 ctx.restore();
+
+// const BigInteger = require('big-integer');
+const DICTIONARY = 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijkmnopqrstuvwxyz23456789';
+// const DICTIONARY_LENGTH = BigInt(DICTIONARY.length);
+const DICTIONARY_LENGTH = BigInt(DICTIONARY.length);
+const SHARECODE_PATTERN = /CSGO(-?[\w]{5}){5}$/;
+
+function bytesToHex(byteArray) {
+    return Array.from(byteArray, function(byte) {
+      return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+  }
+
+function dec2bin(dec){
+    return (dec >>> 0).toString(2);
+}
+
+function h2d(s) {
+
+    function add(x, y) {
+        var c = 0, r = [];
+        var x = x.split('').map(Number);
+        var y = y.split('').map(Number);
+        while(x.length || y.length) {
+            var s = (x.pop() || 0) + (y.pop() || 0) + c;
+            r.unshift(s < 10 ? s : s - 10); 
+            c = s < 10 ? 0 : 1;
+        }
+        if(c) r.unshift(c);
+        return r.join('');
+    }
+
+    var dec = '0';
+    s.split('').forEach(function(chr) {
+        var n = parseInt(chr, 16);
+        for(var t = 8; t; t >>= 1) {
+            dec = add(dec, dec);
+            if(n & t) dec = add(dec, '1');
+        }
+    });
+    return dec;
+}
+
+const encode = (crosshair) => {
+    let bytes = [
+        1,
+        (crosshair.gap * 10),
+        Math.min(6, crosshair.outlineThickness * 2),
+        crosshair.r,
+        crosshair.g,
+        crosshair.b,
+        crosshair.alpha,
+        7,
+        30,
+        53,
+    ];
+    bytes.splice(9, 0, parseInt(`1010${crosshair.outline ? 1 : 0}${dec2bin(crosshair.color).padStart(3, '0')}`, 2));
+    bytes.push(Math.min(63, crosshair.thickness * 10));
+    bytes.push(parseInt(`${crosshair.t ? 1 : 0}${crosshair.useAlpha ? 1 : 0}${crosshair.useWeaponValue ? 1 : 0}${crosshair.dot ? 1 : 0}${dec2bin(crosshair.style).padStart(3, '0')}0`, 2));
+    bytes.push(crosshair.size * 10);
+    bytes = bytes.concat([0, 0, 0]);
+    let checksum = dec2bin(bytes.reduce((a, v) => a + v)).padStart(3, '0');
+    checksum = checksum.substring(checksum.length - 8, checksum.length);
+    bytes.unshift(parseInt(checksum, 2));
+    const hex = bytesToHex(bytes);
+
+    console.log(typeof(hex))
+    console.log(Number.MAX_SAFE_INTEGER)
+    let hex2 = h2d(hex);
+    let total = BigInt(hex2);
+    console.log(total);
+    let c = '';
+    let rem = 0 + 'n';
+    Array.from({ length: 25 }).forEach(() => {
+        rem = total % (DICTIONARY_LENGTH);
+        c += DICTIONARY[rem];
+        total = total /DICTIONARY_LENGTH;
+    });
+    return `CSGO-${c.substring(0, 5)}-${c.substring(5, 10)}-${c.substring(10, 15)}-${c.substring(15, 20)}-${c.substring(20, 25)}`;
+};
+
+function stringToByteArray(str) {
+    const bytes = [];
+    for (let i = 0; i < str.length; i += 2) {
+        bytes.push(parseInt(str.slice(i, i + 2), 16));
+    }
+    return bytes;
+}
+
+
+const decode = (code) => {
+    if (code === undefined || !code.match(SHARECODE_PATTERN)) {
+        return null;
+    }
+    try {
+        const shareCode = code.replace(/CSGO|-/g, '');
+        const chars = Array.from(shareCode).reverse();
+        let big = BigInt(0);
+        chars.forEach((_, i) => {
+            big = big *DICTIONARY_LENGTH  + BigInt(DICTIONARY.indexOf(chars[i]));
+        });
+        const str = big.toString(16).padStart(36, '0');
+        const bytes = stringToByteArray(str);
+        const crosshair = {
+            alpha: 0,
+            b: 0,
+            color: 0,
+            dot: false,
+            g: 0,
+            gap: 0,
+            outline: false,
+            outlineThickness: 0,
+            r: 0,
+            size: 0,
+            style: 0,
+            t: false,
+            thickness: 0,
+            useAlpha: false,
+            useWeaponValue: false
+        };
+        let byte2 = dec2bin(bytes[2]);
+        if (byte2.length === 8 && byte2.startsWith('1')) {
+            byte2 = byte2.padStart(32, '1');
+            crosshair.gap = Math.ceil(~~parseInt(byte2, 2) / 10);
+        }
+        else {
+            crosshair.gap = Math.floor(bytes[2] / 10);
+        }
+        crosshair.gap = crosshair.gap;
+        crosshair.outlineThickness = bytes[3] / 2;
+        crosshair.r = bytes[4];
+        crosshair.g = bytes[5];
+        crosshair.b = bytes[6];
+        crosshair.alpha = bytes[7];
+        const byte10 = dec2bin(bytes[10]);
+        crosshair.outline = parseInt(byte10.substring(4, 5), 2) === 1;
+        crosshair.color = parseInt(byte10.substring(5, 8), 2);
+        crosshair.thickness = bytes[12] / 10;
+        const byte13 = dec2bin(bytes[13]).padStart(8, '0');
+        crosshair.t = parseInt(byte13.substring(0, 1), 2) === 1;
+        crosshair.useAlpha = parseInt(byte13.substring(1, 2), 2) === 1;
+        crosshair.useWeaponValue = parseInt(byte13.substring(2, 3), 2) === 1;
+        crosshair.dot = parseInt(byte13.substring(3, 4), 2) === 1;
+        crosshair.style = parseInt(byte13.substring(4, 7), 2);
+        const byte14 = dec2bin(bytes[14]).padStart(8, '0');
+        const byte15 = dec2bin(bytes[15]);
+        crosshair.size = Math.ceil(parseInt(byte15 + byte14, 2) / 10);
+        return crosshair;
+    }
+    catch (e) {
+        return null;
+    }
+};
+
+let csgo_share_code  = document.getElementById('csgo-share-code');
+
+document.body.append(encode({
+    useAlpha: true,
+    alpha: 255,
+    color: 1,
+    r: 0,
+    g: 0,
+    b: 0,
+    size: 4,
+    style: 4,
+    gap: 0,
+    thickness: 1,
+    dot: false,
+    outline: false,
+    outlineThickness: 1,
+    t: false,
+    useWeaponValue: false
+}));
+
+let csgo_code  = document.getElementById('csgo--code');
+let cr = decode('CSGO-qBD4P-q9uNy-xrYjf-QrW5O-BaBpF');
+document.body.append(cr.alpha);
+document.body.append(cr.color);
+document.body.append(cr.b);
+document.body.append(cr.dot);
+document.body.append(cr.g);
+document.body.append(cr.gap);
+document.body.append(cr.outline);
+document.body.append(cr.outlineThickness);
+document.body.append(cr.r);
+document.body.append(cr.size);
+document.body.append(cr.style);
+document.body.append(cr.t);
